23fa9fce94a9f673685f2c2776e848de33f1c0cc
vymuller <62063595+vymuller@users.noreply.github.com>
2020-03-11 12:55:26 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function setup() {
 function draw() {
   colorMode(HSB,360,100,100);
   //background(0,100,100);
-  fill(random(0,40),100,100);
+  fill(random(0,200),10,100);
   noStroke();
   c = a * random(1,5);
   ellipse(mouseX,mouseY,c,c);
